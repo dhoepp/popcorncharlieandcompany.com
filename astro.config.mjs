c3c676f869e4d995cbdc26d14b92e20eeba01d40
Dustin Hoeppner <dhoepp@me.com>
2026-04-23 18:21:57 -0500

    update site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://popcorncharlieandcompany.com',
+  site: 'https://dhoepp.github.io',
+  base: '/popcorncharlieandcompany.com',
   output: 'static',
 });
